3947dfe1ecd06d5b2382f4a7ad26a162c7d563d9
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-23 23:18:15 -0600

    feat: se ha agregado las dependencias y config del eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        document: "readonly",
+        window: "readonly",
+        fetch: "readonly",
+        console: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      eqeqeq: "error",
+      "prefer-const": "warn",
+    },
+  },
+];
